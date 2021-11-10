8e0a361fc89afb79c5525eccbabec5bbba8fbfa7
karthikeyan <karthirm90@gmail.com>
2021-11-10 21:40:31 +0800

    google analytics change

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ import "../styles/argon-design-system-react.css";
 import ReactGA from 'react-ga';
 import React, {useEffect} from 'react'
 
+useEffect(() => {
+ReactGA.initialize('UA-212470797-1');
+ReactGA.pageview(window.location.pathname + window.location.search)
+},[]);
+
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
-useEffect(() => {
-ReactGA.initialize('UA-212470797-1');
-ReactGA.pageview(window.location.pathname + window.location.search)
-},[]);
