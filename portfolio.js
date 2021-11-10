2f11cd6815444673a1a5987f91473985739b5d88
karthikeyan <karthirm90@gmail.com>
2021-11-10 14:40:51 +0800

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,234 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Karthikeyan Ramanathan",
+	title: "Hi all, I'm Karthi",
+	description:
+		"A passionate and motivated Java-Spring Boot Backend Web Developer and can work as a full stack Developer with Angular being the front-end, having an experience of building Web applications with Java / Spring / SpringBoot / Hibernate / JavaScript and some other cool libraries and frameworks",
+	resumeLink: "",
+};
+
+export const openSource = {
+	githubUserName: "karthirm",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/karthirm",
+	instagram: "",
+	twitter: "https://twitter.com/karthirm",
+	github: "https://github.com/karthirm",
+	linkedin: "https://www.linkedin.com/in/karthikeyan-ramanathan-28636113/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"An EVER-IMPROVING BACK END DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK AND LEARNING ANGULAR",
+	skills: [
+		emoji(
+			"⚡ Develop highly available and scalable back-end stack for web applications"
+		),
+		emoji(
+			"⚡ Possess good knowledge in Java, Spring Boot, Hibernate, Microservices"
+		),
+		emoji(
+			"⚡ Deployed applications into third party services such as Heroku / Google Cloud"
+		),
+		emoji(
+			"⚡ Possess sufficient knowledge in Docker, Openshift & Kubernetes to work on containerization and orchestration of microservices"
+		),
+		emoji(
+			"⚡ Have worked on integrating the applications with CI/CD - Bitbucket-Jenkins as well Github-Heroku"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "TypeScript",
+			fontAwesomeClassname: "logos:typescript-icon",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "swift",
+			fontAwesomeClassname: "vscode-icons:file-type-swift",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Backend", //Insert stack or technology you have experience in
+		progressPercentage: "80", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Frontend",
+		progressPercentage: "50",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Raj Matriculation",
+		subHeader: "Master of Science in Computer Science",
+		duration: "June 1994 - April 2008",
+		desc: "Did my schooling",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		schoolName: "PSG College Of Technology, Coimbatore",
+		subHeader: "Master of Science in Theoretical Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "A research oriented course for a span of 5 years with biggest accomplishment being a part of a research work published in a switzerland journal",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "FIS Global",
+		companylogo: "/img/icons/common/fisLogo.jpeg",
+		date: "September 2018 – Present",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		role: "Associate Developer",
+		company: "CTS",
+		companylogo: "/img/icons/common/ctsLogo.png",
+		date: "October 2013 – August 2018",
+		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Tecnotree",
+		companylogo: "/img/icons/common/tecnotreeLogo.png",
+		date: "Nov 2012 – Apr 2013",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Research Intern",
+		company: "IIT Madras, Chennai",
+		companylogo: "/img/icons/common/IITLogo.png",
+		date: "Apr 2011 – Nov 2011",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+];
+
+export const projects = [
+	{
+		name: "Corona Tracker",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://corona-tracker-app-1.herokuapp.com/",
+	},
+	{
+		name: "Employee Management System",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://springboot-thymeleaf-app.herokuapp.com/employees/getAll",
+		link: "https://github.com/karthirm/spring-boot-thymeleaf"
+
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "",
+		link: "",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Clark Linda",
+		feedback:
+			"Karthik has been an asset for our project. His leadership capabilities has helped us to resolve downtime issues in our project",
+	},
+	{
+		name: "Demarino Vincent",
+		feedback:
+			"Played a very big role in moving to the new stack which deploys our application into Openshift/Docker",
+	},
+	{
+		name: "Susan",
+		feedback:
+			"Excel macro tools which were developed by Karthik has reduced the manual effort by more than 300 hours for our team, well done",
+	},
+	{
+		name: "Hari",
+		feedback:
+			"Karthik has been really innovative and have resolved a lot of production batch job issues for healthcare domain. great job and keep it up",
+	},
+];
