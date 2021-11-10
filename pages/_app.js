34538cb61b197b9947d22ddc8ebdabc4c6a0b26c
karthikeyan <karthirm90@gmail.com>
2021-11-10 21:44:01 +0800

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/vendor/nucleo/css/nucleo.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/argon-design-system-react.css";
-import ReactGA from 'react-ga';
-import React, {useEffect} from 'react'
-
-useEffect(() => {
-ReactGA.initialize('UA-212470797-1');
-ReactGA.pageview(window.location.pathname + window.location.search)
-},[]);
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
