5c7f1b45db431239e3054a1b45e7d4f95273599b
karthikeyan <karthirm90@gmail.com>
2021-11-10 22:06:43 +0800

    added fb

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import React, {useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
+}
+
+function Form(){
 	useEffect(() => {
 	ReactGA.initialize('UA-212470797-1');
 	ReactGA.pageview(window.location.pathname + window.location.search)
